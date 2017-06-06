0c26e5ac3d3853ba087891bb4d8eef487a4c85ec
DanielLChang <daniellawrencechang@gmail.com>
2017-06-05 19:00:26 -0700

    Animate triangle

--- TriangleAnimation/main.js
+++ TriangleAnimation/main.js
@@ -1,17 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.getElementById('btn');
+  let height = 0;
+  let width = 0;
+  const color = '#' + Math.floor(Math.random()*16777215).toString(16);
+
+  function animate() {
+    const triangle = document.getElementById('triangle');
+
+    height += 5;
+    width += 5;
+
+    triangle.style.borderBottom = `${height}px solid ${color}`;
+    triangle.style.borderLeft = `${width}px solid transparent`;
+
+    if (height <= window.innerHeight * 2
+      || width <= window.innerWidth * 2) setTimeout(() => animate(), 10);
+  }
 
   btn.addEventListener('click', () => {
     const triangle = document.createElement('div');
+    triangle.id = 'triangle';
     triangle.style.position = 'absolute';
     triangle.style.width = 0;
     triangle.style.height = 0;
+    triangle.style.bottom = 0;
+    triangle.style.right = 0;
     // triangle.style.borderBottom = '100px solid black';
     // triangle.style.borderLeft = '100px solid transparent';
 
-    triangle.style.bottom = 0;
-    triangle.style.right = 0;
 
     document.body.appendChild(triangle);
+
+    animate(triangle);
   });
 });
